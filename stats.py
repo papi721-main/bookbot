9a21da55f2f412eb02842134bdab6e864c965298
papi721-main <99392252+papi721-main@users.noreply.github.com>
2025-04-24 13:05:07 +0300

    feat: implement a function that returns sorted list of dictionaries for the character count

--- stats.py
+++ stats.py
@@ -40,3 +40,29 @@ def get_character_counts(text: str):
         else:
             character_count[ch] = 1
     return character_count
+
+
+def sort_on_count(count_dict: dict):
+    """Takes a dictionary returns the value of the 'count' key"""
+    return count_dict["count"]
+
+
+def get_sorted_character_counts_list(character_count: dict):
+    """
+    Takes the dictionary of characters and their counts and returns a sorted
+    list of dictionaries
+
+    Args:
+        character_count (dict): dictionary containing the count of each
+        character in the text
+
+    Returns:
+        list: a list of dictionaries containing the count of each
+        character in the text, sorted by the character count
+    """
+    character_count_list = [
+        {"char": ch, "count": character_count[ch]} for ch in character_count
+    ]
+
+    character_count_list.sort(key=sort_on_count, reverse=True)
+    return character_count_list
